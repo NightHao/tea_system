c0cb6857d5ed5c1dd5762f80e32ed45fdd22cc38
markhsumark <tulsa0415@gmail.com>
2022-01-06 19:04:43 +0800

    updata index.html , add search.php

--- teasystem_program/drink.php
+++ teasystem_program/drink.php
@@ -8,79 +8,100 @@
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <title>台灣茶系統</title>
     </head>
+    <script>
+        $(document).ready(function(){
+        $("#myInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+        });
+    </script>
     <center>
-        <body>
-            <div class="jumbotron text-center", id="bar">
-                <h1>台灣手搖茶系統</h1>
-                <p>喝......茶......</p> 
-            </div>
-            <ul class="nav nav-tabs nav-justified" >
-                <li class="nav-item col-md-2">
-                    <a class="nav-link active " href="index.html">首頁</a>
+    <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+            
+            <ul class="nav navbar-nav" >
+                <li class="nav-item">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="img/cuplogo.png" alt="Logo" style="width:40px;">
+                    </a>
                 </li>
-                <!-- <li class="nav-item col-md-2 ">
-                    <a class="nav-link clickable" href="#" >搜尋</a>
-                </li> -->
-                <li class="nav-item col-md-2">
-                    <a class="nav-link clickable" href="edit.html">編輯</a>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">首頁</a>
                 </li>
-                
+                <li class="nav-item">
+                    <a class="nav-link" href="edit.html">編輯</a>
+                </li>
             </ul>
-            <div class='container', style= 'margin-top: 10px;'>
-                <table class='block'>
-                    <tr>
-                        <th>品牌名稱</th>
-                        <th>商家名稱</th>
-                        <th>飲料種類</th>
-                        <th>飲料名稱</th>
-                        <th>價錢</th>
-                        <th>大小</th>
-                        <th>地址</th>
-                        <th>營業時間</th>
-                        <th>備註</th>
-                    </tr>
-                    <?php
-                        header("Content-type:text/html;charset=utf-8");
-                        include "db_conn.php";
-                        $shopname = "";
-                        $query = ("select * from drink natural left outer join brand natural join drinkshop");
-                        $stmt = $db->prepare($query);
-                        $error = $stmt->execute();
-                        $result = $stmt->fetchAll();
-                        for($i=0; $i < count($result); $i++){
-                            $brandname = $result[$i]['brandname'];
-                            $shopname = $result[$i]['shopname'];
-                            $category = $result[$i]['category'];
-                            $name = $result[$i]['name'];
-                            $price = $result[$i]['price'];
-                            $size = $result[$i]['size'];
-                            $address = $result[$i]['address'];
-                            $time = $result[$i]['time'];
-                            $info = $result[$i]['info'];
-                            newRow($brandname, $shopname, $category, $name, $price, $size, $address, $time, $info);
-                        }
+        </nav>
+        <div class='container-fluid' style= 'margin-top: 10px;'>
+        <div class="col-md-4" style="text-align:right;">
+            <p>關鍵字搜尋</p>
+            <input class="form-control" id="myInput" type="text" placeholder="Search..">
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <table class='block table col-lg'>
+                    <thaed>
+                        <tr>
+                            <th class="font-weight-bolder">品牌名稱</th>
+                            <th class="font-weight-bolder">商家名稱</th>
+                            <th class="font-weight-bolder">飲料種類</th>
+                            <th class="font-weight-bolder">飲料名稱</th>
+                            <th class="font-weight-bolder">價錢</th>
+                            <th class="font-weight-bolder">大小</th>
+                            <th class="font-weight-bolder">地址</th>
+                            <th class="font-weight-bolder">營業時間</th>
+                            <th class="font-weight-bolder">備註</th>
+                        </tr>
+                    </thead>
+                    <tbody id="myTable">
+                        <?php
+                            header("Content-type:text/html;charset=utf-8");
+                            include "db_conn.php";
+                            $shopname = "";
+                            $query = ("select * from drink natural left outer join brand natural join drinkshop");
+                            $stmt = $db->prepare($query);
+                            $error = $stmt->execute();
+                            $result = $stmt->fetchAll();
+                            for($i=0; $i < count($result); $i++){
+                                $brandname = $result[$i]['brandname'];
+                                $shopname = $result[$i]['shopname'];
+                                $category = $result[$i]['category'];
+                                $name = $result[$i]['name'];
+                                $price = $result[$i]['price'];
+                                $size = $result[$i]['size'];
+                                $address = $result[$i]['address'];
+                                $time = $result[$i]['time'];
+                                $info = $result[$i]['info'];
+                                newRow($brandname, $shopname, $category, $name, $price, $size, $address, $time, $info);
+                            }
 
 
-                        function newRow($brandname, $shopname, $category, $name, $price, $size, $address, $time, $info){
-                            echo "<tr>";
-                            echo "<td>".$brandname."</td>";
-                            echo "<td>".$shopname."</td>";
-                            echo "<td>".$category."</td>";
-                            echo "<td>".$name."</td>";
-                            echo "<td>".$price."</td>";
-                            echo "<td>".$size."</td>";
-                            echo "<td>".$address."</td>";
-                            echo "<td>".$time."</td>";
-                            echo "<td>".$info."</td>";
-                            echo "</tr>.";
-                        }
-                    ?>
+                            function newRow($brandname, $shopname, $category, $name, $price, $size, $address, $time, $info){
+                                echo "<tr>";
+                                echo "<td>".$brandname."</td>";
+                                echo "<td>".$shopname."</td>";
+                                echo "<td>".$category."</td>";
+                                echo "<td>".$name."</td>";
+                                echo "<td>".$price."</td>";
+                                echo "<td>".$size."</td>";
+                                echo "<td>".$address."</td>";
+                                echo "<td>".$time."</td>";
+                                echo "<td>".$info."</td>";
+                                echo "</tr>";
+                            }
+                        ?>
+                    </tbody>
                 </table>
-                <br>
-                    <input type = 'button' onclick='history.back()' value='Go Back' class="btn clickable"></input>
             </div>
-        </body>
+        </div>
+        <input type = 'button' onclick='history.back()' value='回首頁' class="btn clickable rounded-circle" style="position: fixed; bottom: 40px;left: 40px;"></input>
+    </body>
     </center>
 </html>
 
