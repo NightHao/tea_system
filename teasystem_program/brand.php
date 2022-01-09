a564a7fc8260515f1968f6e460e8780c3b30df12
Jason Chen <a0973471825@gmail.com>
2022-01-09 16:44:32 +0800

    plus brand.php

---
+++ teasystem_program/brand.php
@@ -0,0 +1,95 @@
+<?php
+    header("Content-type:text/html;charset=utf-8");
+    include "db_conn.php";
+    
+    echo "<table border='1'>
+    <tr>
+    <th>品牌名稱</th>
+    <th>商家數量</th>
+    </tr>";
+
+    $brandname = "";
+    $query = ("select brandname,count(shopname) as count_shop from brand group by brandname");
+    $stmt = $db->prepare($query);
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();
+    
+    for($i=0; $i < count($result); $i++){
+    echo "<tr>";
+    echo "<td>".$result[$i]['brandname']."</td>";
+    echo "<td>".$result[$i]['count_shop']."</td>";
+    echo "</tr>.";
+    }
+	 
+	 
+echo "</table>";
+
+    echo "<table border='1'>
+    <tr>
+    <th>店名</th>
+    <th>地址</th>
+    </tr>";
+
+    $brandname = "";
+    $query = ("select shopname,address from drinkshop where brandname = '50嵐' ");
+    $stmt = $db->prepare($query);
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();
+    
+    for($i=0; $i < count($result); $i++){
+    echo "<tr>";
+    echo "<td>".$result[$i]['shopname']."</td>";
+    echo "<td>".$result[$i]['address']."</td>";
+    echo "</tr>.";
+    }
+	 
+	 
+echo "</table>";
+
+    echo "<table border='1'>
+    <tr>
+    <th>店名</th>
+    <th>地址</th>
+    </tr>";
+
+    $brandname = "";
+    $query = ("select shopname,address from drinkshop where brandname = 'YK' ");
+    $stmt = $db->prepare($query);
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();
+    
+    for($i=0; $i < count($result); $i++){
+    echo "<tr>";
+    echo "<td>".$result[$i]['shopname']."</td>";
+    echo "<td>".$result[$i]['address']."</td>";
+    echo "</tr>.";
+    }
+	
+	
+echo "</table>";
+
+echo "</table>";
+
+    echo "<table border='1'>
+    <tr>
+    <th>店名</th>
+    <th>地址</th>
+    </tr>";
+
+    $brandname = "";
+    $query = ("select shopname,address from drinkshop where brandname = '珍煮丹' ");
+    $stmt = $db->prepare($query);
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();
+    
+    for($i=0; $i < count($result); $i++){
+    echo "<tr>";
+    echo "<td>".$result[$i]['shopname']."</td>";
+    echo "<td>".$result[$i]['address']."</td>";
+    echo "</tr>.";
+    }
+	
+	
+echo "</table>";
+echo "<br><input type = 'button' onclick='history.back()' value='Go Back'></input>"
+?>
